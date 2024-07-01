237ec528f0a3cb2ceec04d2b9e60214be0836bc4
Ales-Bas <baskakov.a.a@yandex.ru>
2024-07-01 23:44:46 +0300

    add email nullstring

--- src/service/user/type.ts
+++ src/service/user/type.ts
@@ -9,7 +9,7 @@ export const ROLES: Record<Role, Role> = {
 
 export type UserEntity = {
     id: UserId;
-    email: string;
+    email: NullString;
     role: Role;
     emailVerified?: Date | null;
     name?: NullString;
